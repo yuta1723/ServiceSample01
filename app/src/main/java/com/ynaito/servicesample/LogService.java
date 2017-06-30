5c167068af7cfdfc15cfb98fea49b0a6acc5cbe7
yuta naito <y.naito@suvt.co.jp>
2017-06-30 15:20:45 +0900

    first commit

---
+++ app/src/main/java/com/ynaito/servicesample/LogService.java
@@ -0,0 +1,52 @@
+package com.ynaito.servicesample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by y.naito on 2017/06/30.
+ */
+
+public class LogService extends Service {
+
+    private String TAG = LogService.class.getSimpleName();
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.d(TAG, "onRebind");
+        super.onRebind(intent);
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        super.onCreate();
+    }
+}
